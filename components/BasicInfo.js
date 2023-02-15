8002ebd9f55bc9b87be47498136688fc9805f447
Josh Dixon <joshdixon1995@hotmail.co.uk>
2023-02-15 01:07:03 +0000

    image config

--- components/BasicInfo.js
+++ components/BasicInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Date from "./Date";
+import Image from "next/image";
 import styles from '../styles/BasicInfo.module.scss';
 import { HiExternalLink } from 'react-icons/hi'
 
@@ -11,12 +12,12 @@ const BasicInfo = ({ user }) => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.avatar}>
-                <img src={user.avatar} alt="avatar" />
+                <Image src={user.avatar} alt="avatar" width={100} height={100} />
             </div>
 
             <div className={styles.infoWrapper}>
                 <span className={styles.name}>{user.name}</span>
-                <a href={ghProfile} target='_blank'>
+                <a href={ghProfile} target='_blank' rel='noreferrer'>
                     <span className={styles.username}>{user.username} <HiExternalLink /></span>
                 </a>
                 <Date dateString={user.regdate} />
